78a758878d3bf7ed29d6620a6d75c1214250fa2b
NanQi <nanqi@my2space.com>
2017-12-19 11:06:21 +0800

    替换域名

--- controllers/site.js
+++ controllers/site.js
@@ -135,7 +135,7 @@ exports.sitemap = function (req, res, next) {
           return next(err);
         }
         topics.forEach(function (topic) {
-          urlset.ele('url').ele('loc', 'http://forum.jobchain.xyz/topic/' + topic._id);
+          urlset.ele('url').ele('loc', 'http://forum.my2space.com/topic/' + topic._id);
         });
 
         var sitemapData = urlset.end();
